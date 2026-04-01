e7d2ff6fa3e13bd4c1c11a5fc84fd3fec4e94d73
Eati Nikhilesh <22bcs039@iiitdwd.ac.in>
2026-04-02 00:36:09 +0530

    added the room creation logic, dashboardlogic, interview room slug

---
+++ code/src/app/dashboard/page.tsx
@@ -0,0 +1,60 @@
+
+
+import {db} from "@/lib/db";
+import { createRoom } from "@/server/room-actions";
+import { notFound } from "next/navigation";
+
+export default async function Dashboard()
+{
+    // fetch the dummy user from the db
+    let user;
+
+    try
+    {
+        user = await db.user.findUnique({
+        where: {
+            email: "spiderman@gmail.com"
+            }
+        });
+    }
+    catch(E)
+    {
+        console.log(E);
+        throw new Error;
+    }
+    
+    if(user==null)
+    {
+        // <h2>Dummy user not found in the db</h2>
+        notFound();
+    }
+
+    // create an interview-room with the dummy user, use the createRoom
+
+    const createRoomHandler =  async () =>{
+
+        try
+        {
+            "use server"
+           await createRoom(user.id)
+        }
+        catch(e)
+        {
+            console.log(e);
+            throw new Error;
+        }   
+    };
+
+    return(
+        <div className="p-20">
+      <h1>Welcome {user.name}</h1>
+      
+      {/* The form 'action' triggers our function only on click */}
+      <form action={createRoomHandler}>
+        <button className="bg-indigo-600 text-white p-4 rounded">
+          Create Room
+        </button>
+      </form>
+    </div>
+    );
+}
--- code/src/server/room-actions.ts
+++ code/src/server/room-actions.ts
@@ -1,5 +1,43 @@
 
+"user servers"
+
 import {db} from '@lib/db';
 import {nanoid} from 'nanoid';
-import 
+import {redirect} from 'next/navigation';
+
+export async function createRoom(userId: string)
+{
+    const slug = nanoid(10); //slug used to identify the room
+
+    try
+    { // creating a room in the db
+        const newRoom = await db.room.create({
+            data : {
+                slug: slug,
+                createdById: userId,
+                language: "javascript"
+            }
+        });
+        
+        // add the user who created the room as interviewer into participants.
+        await db.participant.create({
+            
+            data: {
+                userId: userId,
+                roomId: newRoom.id,
+                role: "interviewer"
+            }
+
+        });
+
+    }
+    catch(error)
+    {
+        console.log("Failed to create a room! " + error);
+        throw new Error("Database error: Could not create the interview room.");
+    }
+
+    //  Redirect the user to the dynamic route: /room/[slug], upon creation of room
+    redirect(`/room/${slug}`);
+};
 
