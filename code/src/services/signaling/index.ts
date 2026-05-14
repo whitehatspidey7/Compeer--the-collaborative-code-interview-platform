e99ea6c957b1e8f3461a2ddae377671ffccb865f
Eati Nikhilesh <22bcs039@iiitdwd.ac.in>
2026-05-15 00:43:33 +0530

    integrated Yjs to the signaling server

--- code/src/services/signaling/index.ts
+++ code/src/services/signaling/index.ts
@@ -1,18 +1,23 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import * as Y from 'yjs';
 
 // 1. DEFINE THE CONTRACT (The TypeScript Magic)
 // What the frontend is allowed to send TO the server
 interface ClientToServerEvents {
   "join-room": (data: { slug: string }) => void;
-  "code-change": (data: { slug: string; code: string }) => void;
+  "yjs-update": (data: { slug: string; code: Uint8Array }) => void;
 }
 
 // What the server is allowed to send TO the frontend
 interface ServerToClientEvents {
-  "receive-code": (code: string) => void;
+  "yjs-update": (code: Uint8Array) => void;
 }
+
+// map with list of all the documents of all the interview rooms
+const documents  =  new Map<string , Y.Doc>();
+
 
 const app = express();
 const server = createServer(app); //http server
@@ -26,16 +31,39 @@ const io = new Server<ClientToServerEvents, ServerToClientEvents>(server, {
   },
 });
 
+
 io.on("connection", (socket) => {
+
   console.log(" User connected:", socket.id);
+  
 
   socket.on("join-room", ({ slug }) => {
     socket.join(slug);
     console.log(` User ${socket.id} joined room: ${slug}`);
+
+    // if the document for the room doesn't exist , create one
+    if(!documents.has(slug))
+    {
+       documents.set(slug, new Y.Doc);
+    }
+
+    const doc  = documents.get(slug)!;
+    const stateVector = Y.encodeStateAsUpdate(doc);
+
+    socket.emit("yjs-update",stateVector);
+
   });
 
-  socket.on("code-change", ({ slug, code }) => {
-    socket.to(slug).emit("receive-code", code);
+    
+  socket.on("yjs-update", ({ slug, code }) => {
+
+    const doc = documents.get(slug);
+
+    if(doc)
+    {
+        Y.applyUpdate(doc, new Uint8Array(code));
+    }
+    socket.to(slug).emit("yjs-update", code);
   });
 
   socket.on("disconnect", () => {
